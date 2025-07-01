d021172318207601a808f2d6548e6ad0ce8a4706
moabia1 <mdmoabia81@gmail.com>
2025-07-01 10:14:01 +0530

    added new ques

--- day1/script.js
+++ day1/script.js
@@ -1,15 +1,32 @@
 // Ques:- 1 Tower Of Hanoi 
-let n = 3;
-function towerOfHanoi(n, s, h, d) {
-  if (n == 1) {
-    console.log("moved 1 from " + s + " to " + d);
-    return
-  }
-  towerOfHanoi(n - 1, s, d, h);
-  console.log("moved " + n + " plate to " + s + " to " + d);
-  towerOfHanoi(n - 1, h, s, d);
-}
-towerOfHanoi(n, "s", "h", "d");
+//let n = 3;
+// function towerOfHanoi(n, s, h, d) {
+//   if (n == 1) {
+//     console.log("moved 1 from " + s + " to " + d);
+//     return
+//   }
+//   towerOfHanoi(n - 1, s, d, h);
+//   console.log("moved " + n + " plate to " + s + " to " + d);
+//   towerOfHanoi(n - 1, h, s, d);
+// }
+// towerOfHanoi(n, "s", "h", "d");
 
 
 // Ques:- 2  joshephus Problem(find winner of circular game) Lt:-1823
+let n = 5, k = 2;
+
+let solve = (k, arr, i) => {
+  if (arr.length == 1) return arr[0];
+  i = (i + k) % arr.length;
+  arr.splice(i, 1);
+  return solve(k, arr, i);
+}
+let findTheWinner = (n,k) => {
+  let arr = new Array(n);
+  for (let i = 0; i < n; i++){
+    arr[i] = i + 1;
+  }
+  k--
+  return solve(k, arr, 0);
+}
+console.log(findTheWinner(n, k));
---
+++ day2/script.js
@@ -0,0 +1,21 @@
+// Ques :- 1 Subsets Lt:-
+let arr = [1, 2, 3];
+function solve(arr,ans,cur,i) {
+  if (i === arr.length) {
+    ans.push([...cur]);
+    return
+  }
+  //pick
+  cur.push(arr[i]);
+  solve(arr, ans, cur, i + 1);
+  cur.pop();
+  //not pick
+  solve(arr, ans, cur, i + 1);
+}
+function subset(arr) {
+  let ans = []
+  let cur = []
+  solve(arr, ans, cur, 0);
+  return ans
+}
+console.log(subset(arr))
