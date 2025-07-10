9e18313d09621456a5693987e76b873df1e171d9
moabia1 <mdmoabia81@gmail.com>
2025-07-10 23:19:39 +0530

    remove comments

--- day2/script.js
+++ day2/script.js
@@ -1,53 +1,53 @@
 // Ques :- 1 Subsets Lt:- 78
 
-// let arr = [1, 2, 3];
-// function solve(arr,ans,cur,i) {
-//   if (i === arr.length) {
-//     ans.push([...cur]);
-//     return
-//   }
-//   //pick
-//   cur.push(arr[i]);
-//   solve(arr, ans, cur, i + 1);
-//   cur.pop();
-//   //not pick
-//   solve(arr, ans, cur, i + 1);
-// }
-// function subset(arr) {
-//   let ans = []
-//   let cur = []
-//   solve(arr, ans, cur, 0);
-//   return ans
-// }
-//console.log(subset(arr))
+let arr = [1, 2, 3];
+function solve(arr,ans,cur,i) {
+  if (i === arr.length) {
+    ans.push([...cur]);
+    return
+  }
+  //pick
+  cur.push(arr[i]);
+  solve(arr, ans, cur, i + 1);
+  cur.pop();
+  //not pick
+  solve(arr, ans, cur, i + 1);
+}
+function subset(arr) {
+  let ans = []
+  let cur = []
+  solve(arr, ans, cur, 0);
+  return ans
+}
+console.log(subset(arr))
 
 
 
 
 // Ques-2 Power Set Gfg
-// let s = "abc";
-// function solve(s, ans, curr, i){
-//   if (i === s.length) {
-//     if (curr.length != 0) {
-//       ans.push(curr)
-//     }
-//     return
-//   }
+let s = "abc";
+function solve(s, ans, curr, i){
+  if (i === s.length) {
+    if (curr.length != 0) {
+      ans.push(curr)
+    }
+    return
+  }
 
-//   //pick
-//   curr += s[i];
-//   solve(s, ans, curr, i + 1);
-//   curr = curr.substring(0, curr.length - 1);
-//   solve(s, ans, curr, i + 1);
-// }
-// function AllPossibleStr(s) {
-//   let ans = []
-//   let curr = "";
-//   solve(s,ans,curr,0)
-//   ans.sort();
-//   return ans
-// }
-//console.log(AllPossibleStr(s));
+  //pick
+  curr += s[i];
+  solve(s, ans, curr, i + 1);
+  curr = curr.substring(0, curr.length - 1);
+  solve(s, ans, curr, i + 1);
+}
+function AllPossibleStr(s) {
+  let ans = []
+  let curr = "";
+  solve(s,ans,curr,0)
+  ans.sort();
+  return ans
+}
+console.log(AllPossibleStr(s));
 
 
 
